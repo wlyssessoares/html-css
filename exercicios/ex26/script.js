0f1730d6009ff8456f6016464fda0105d563004d
Wlysses Soares <wlysses_soares@outlook.com>
2024-05-08 20:14:14 -0300

    Update script.js

--- exercicios/ex26/script.js
+++ exercicios/ex26/script.js
@@ -3,3 +3,24 @@ console.log("Digite o nome do seu jogador")
 let nickname = "Wlysses"
 
 console.log("Bem vindo " + nickname)
+
+
+
+
+console.log( notificacao + "Tem um novo Pokemon Go na região")
+console.log( notificacao + "Você foi derrotado por um líder")
+
+
+
+
+let poteCafe = "Café Pilão"
+let poteAcucar = "Açucar Cristal"
+let poteBiscoito = "Biscoito Maizena"
+
+console.log("Na casa da vovô hoje tem: " +
+poteCafe + " - " + poteAcucar + " - " + poteBiscoito)
+
+poteCafe = " Café 3 Corações"
+
+console.log("Na casa da vovô hoje tem: " +
+poteCafe + " - " + poteAcucar + " - " + poteBiscoito)
